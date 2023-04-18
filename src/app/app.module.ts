bef201a0d9935843cd782cf6c2945a567a27e453
bopdidus <bopdidus@gmail.com>
2023-04-18 15:31:26 +0200

    new Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EinkaufComponent } from './einkauf/einkauf.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EinkaufComponent
   ],
   imports: [
     BrowserModule,
